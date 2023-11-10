015f50ef837fc1643133942eba8bda433e9226e2
lawrence <lordlawrencesalazar@gmail.com>
2023-11-10 20:50:47 +0800

    added alwaffa project

---
+++ projects/alwaffa/about.php
@@ -0,0 +1,316 @@
+
+
+
+
+<script type="text/javascript" src="/static/js/analytics.js"></script>
+<script type="text/javascript">archive_analytics.values.server_name="wwwb-app17.us.archive.org";archive_analytics.values.server_ms=6719;</script>
+<link type="text/css" rel="stylesheet" href="/static/css/banner-styles.css"/>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59983852-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-60160728-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!--[if lt IE 7]><html class="ie9 ie8 ie7 ie6" lang="en"><![endif]-->
+<!--This Website is Created by Lord Lawrence M. Salazar -->
+<!--[if IE 7]><html class="ie9 ie8 ie7" lang="en"><![endif]-->
+<!--[if IE 8]><html class="ie9 ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if gt IE 9]><!--><!--<![endif]-->
+<HTML lang="ar" dir="rtl" xml:lang="ar" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Al-Waffa Printing Press Najran | About us</title>
+	<link rel="icon" type="image/x-icon" href="img/icon/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/slider.css"/>
+	<script type="text/javascript" src="js/jquery-1.5.2.min.js"></script>
+	<script src="js/modernizr.custom.63321.js"></script>
+	<meta charset="UTF-8">
+</head>
+<body>
+
+
+<!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<script type="text/javascript" src="/static/js/disclaim-element.js" ></script>
+<script type="text/javascript" src="/static/js/graph-calc.js" ></script>
+<script type="text/javascript">//<![CDATA[
+var __wm = (function(imgWidth,imgHeight,yearImgWidth,monthImgWidth){
+var wbPrefix = "/web/";
+var wbCurrentUrl = "http://alwaffaprint.com/about";
+
+var firstYear = 1996;
+var displayDay = "14";
+var displayMonth = "Oct";
+var displayYear = "2016";
+var prettyMonths = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+var $D=document,$=function(n){return document.getElementById(n)};
+var trackerVal,curYear = -1,curMonth = -1;
+var yearTracker,monthTracker;
+function showTrackers(val) {
+  if (val===trackerVal) return;
+  var $ipp=$("wm-ipp");
+  var $y=$("displayYearEl"),$m=$("displayMonthEl"),$d=$("displayDayEl");
+  if (val) {
+    $ipp.className="hi";
+  } else {
+    $ipp.className="";
+    $y.innerHTML=displayYear;$m.innerHTML=displayMonth;$d.innerHTML=displayDay;
+  }
+  yearTracker.style.display=val?"inline":"none";
+  monthTracker.style.display=val?"inline":"none";
+  trackerVal = val;
+}
+function trackMouseMove(event,element) {
+  var eventX = getEventX(event);
+  var elementX = getElementX(element);
+  var xOff = Math.min(Math.max(0, eventX - elementX),imgWidth);
+  var monthOff = xOff % yearImgWidth;
+
+  var year = Math.floor(xOff / yearImgWidth);
+  var monthOfYear = Math.min(11,Math.floor(monthOff / monthImgWidth));
+  // 1 extra border pixel at the left edge of the year:
+  var month = (year * 12) + monthOfYear;
+  var day = monthOff % 2==1?15:1;
+  var dateString = zeroPad(year + firstYear) + zeroPad(monthOfYear+1,2) +
+    zeroPad(day,2) + "000000";
+
+  $("displayYearEl").innerHTML=year+firstYear;
+  $("displayMonthEl").innerHTML=prettyMonths[monthOfYear];
+  // looks too jarring when it changes..
+  //$("displayDayEl").innerHTML=zeroPad(day,2);
+  var url = wbPrefix + dateString + '/' +  wbCurrentUrl;
+  $("wm-graph-anchor").href=url;
+
+  if(curYear != year) {
+    var yrOff = year * yearImgWidth;
+    yearTracker.style.left = yrOff + "px";
+    curYear = year;
+  }
+  if(curMonth != month) {
+    var mtOff = year + (month * monthImgWidth) + 1;
+    monthTracker.style.left = mtOff + "px";
+    curMonth = month;
+  }
+}
+function hideToolbar() {
+  $("wm-ipp").style.display="none";
+}
+function bootstrap() {
+  var $spk=$("wm-ipp-sparkline");
+  yearTracker=$D.createElement('div');
+  yearTracker.className='yt';
+  with(yearTracker.style){
+    display='none';width=yearImgWidth+"px";height=imgHeight+"px";
+  }
+  monthTracker=$D.createElement('div');
+  monthTracker.className='mt';
+  with(monthTracker.style){
+    display='none';width=monthImgWidth+"px";height=imgHeight+"px";
+  }
+  $spk.appendChild(yearTracker);
+  $spk.appendChild(monthTracker);
+
+  var $ipp=$("wm-ipp");
+  $ipp&&disclaimElement($ipp);
+}
+return{st:showTrackers,mv:trackMouseMove,h:hideToolbar,bt:bootstrap};
+})(550, 27, 25, 2);//]]>
+</script>
+<style type="text/css">
+body {
+  margin-top:0 !important;
+  padding-top:0 !important;
+  min-width:800px !important;
+}
+</style>
+<div id="wm-ipp" lang="en" style="display:none;">
+
+<div style="position:fixed;left:0;top:0;width:100%!important">
+<div id="wm-ipp-inside">
+   <table style="width:100%;"><tbody><tr>
+   <td id="wm-logo">
+       <a href="/web/" title="Wayback Machine home page"><img src="/static/images/toolbar/wayback-toolbar-logo.png" alt="Wayback Machine" width="110" height="39" border="0" /></a>
+   </td>
+   <td class="c">
+       <table style="margin:0 auto;"><tbody><tr>
+       <td class="u" colspan="2">
+       <form target="_top" method="get" action="/web/form-submit.jsp" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="http://alwaffaprint.com/about" style="width:400px;" onfocus="this.focus();this.select();" /><input type="hidden" name="type" value="replay" /><input type="hidden" name="date" value="20161014152657" /><input type="submit" value="Go" /><span id="wm_tb_options" style="display:block;"></span></form>
+       </td>
+       <td class="n" rowspan="2">
+           <table><tbody>
+           <!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
+           <tr class="m">
+           	<td class="b" nowrap="nowrap">
+		
+		    <a href="/web/20150802003221/http://www.alwaffaprint.com/about" title="2 Aug 2015">AUG</a>
+		
+		</td>
+		<td class="c" id="displayMonthEl" title="You are here: 15:26:57 Oct 14, 2016">OCT</td>
+		<td class="f" nowrap="nowrap">
+		
+		    Nov
+		
+                </td>
+	    </tr>
+           <!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
+           <tr class="d">
+               <td class="b" nowrap="nowrap">
+               
+                   <a href="/web/20150802003221/http://www.alwaffaprint.com/about" title="0:32:21 Aug 2, 2015"><img src="/static/images/toolbar/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0" /></a>
+               
+               </td>
+               <td class="c" id="displayDayEl" style="width:34px;font-size:24px;" title="You are here: 15:26:57 Oct 14, 2016">14</td>
+	       <td class="f" nowrap="nowrap">
+               
+                   <img src="/static/images/toolbar/wm_tb_nxt_off.png" alt="Next capture" width="14" height="16" border="0"/>
+               
+	       </td>
+           </tr>
+           <!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
+           <tr class="y">
+	       <td class="b" nowrap="nowrap">
+               
+                   <a href="/web/20150802003221/http://www.alwaffaprint.com/about" title="2 Aug 2015"><strong>2015</strong></a>
+               
+               </td>
+               <td class="c" id="displayYearEl" title="You are here: 15:26:57 Oct 14, 2016">2016</td>
+	       <td class="f" nowrap="nowrap">
+               
+                   2017
+               
+	       </td>
+           </tr>
+           </tbody></table>
+       </td>
+       </tr>
+       <tr>
+       <td class="s">
+           <a class="t" href="/web/20161014152657*/http://alwaffaprint.com/about" title="See a list of every capture for this URL">2 captures</a>
+           <div class="r" title="Timespan for captures of this URL">2 Aug 15 - 14 Oct 16</div>
+       </td>
+       <td class="k">
+       <a href="" id="wm-graph-anchor">
+       <div id="wm-ipp-sparkline" title="Explore captures for this URL">
+	 <img id="sparklineImgId" alt="sparklines"
+		 onmouseover="__wm.st(1)" onmouseout="__wm.st(0)"
+		 onmousemove="__wm.mv(event,this)"
+		 width="550"
+		 height="27"
+		 border="0"
+		 src="/web/jsp/graph.jsp?graphdata=550_27_1996:-1:000000000000_1997:-1:000000000000_1998:-1:000000000000_1999:-1:000000000000_2000:-1:000000000000_2001:-1:000000000000_2002:-1:000000000000_2003:-1:000000000000_2004:-1:000000000000_2005:-1:000000000000_2006:-1:000000000000_2007:-1:000000000000_2008:-1:000000000000_2009:-1:000000000000_2010:-1:000000000000_2011:-1:000000000000_2012:-1:000000000000_2013:-1:000000000000_2014:-1:000000000000_2015:-1:000000010000_2016:9:000000000100_2017:-1:000000000000" />
+       </div>
+       </a>
+       </td>
+       </tr></tbody></table>
+   </td>
+   <td class="r">
+       <a href="#close" onclick="__wm.h();return false;" style="background-image:url(/static/images/toolbar/wm_tb_close.png);top:5px;" title="Close the toolbar">Close</a>
+       <a href="http://faq.web.archive.org/" style="background-image:url(/static/images/toolbar/wm_tb_help.png);bottom:5px;" title="Get some help using the Wayback Machine">Help</a>
+   </td>
+   </tr></tbody></table>
+</div>
+</div>
+</div>
+<script type="text/javascript">__wm.bt();</script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+
+	<div class="head">
+		<div id="inside-head">
+			<div id="logo">
+				<img src="img/logo.png"/>
+			</div>
+			<div id="comp-name">
+				<h1>مطابع الوفاء</h1>
+				<hr>
+				<h2>Al-Waffa Printing Press</h2>
+			</div>
+			<div style="font-family:'Ubuntu';text-align:left;color:#ccc;">
+				<span style="background:#fff;border-radius:5px;padding-bottom:2px;padding-right:5px;color:#8f1f24;">العربية </span></a> &nbsp;| <a style="color:#ccc;" href="eng/about">English</a>
+			</div>
+		</div>
+	</div>
+	<div class="body-wrap">
+		<div class="menu-holder">
+			<div id="menu">
+				<ul>
+					<li class="service"> <a href="index">الرئيسية</a>  </li> 
+					<li class="quotation"><a href="about">من نحن</a>  </li> 
+					<li class="home"><a href="service">خدماتنا</a>  </li>
+					<li class="contactus"> <a href="product">منتجاتنا</a>  </li> 
+					<li class="feature"><a href="photos">المطابع في صور</a></li>
+					<li class="feature"><a href="admin">مجلس ادارة المطابع</a></li>
+				</ul>
+			</div>
+		</div>
+					<div style="margin-top:10px;text-align:justify;" id="intro-wrap">
+						<h1>مطابع الوفاء</h1>
+						<p style="padding-left:20px;">بدأت مطابع الوفــاء مسيرتها الناجحة عام 1399هـ كأحد أبرز مزودي حلول وخدات الطباعة الاحترافية المتطورة منطقة نجران ، اعتمدت إدارة المطبعة منذ انطلاقتها مفهوم البحث والدراسة والتحليل العلمي لميول ورغبات العملاء والزبائن، ومواكبة أحدث الاكتشافات والتطبيقات العصرية الحديثة المتطورة في هذا المجال بشكل مستمر؛ واستثمارها في تطوير وحدات عملها عبر كافة مراحلها</p>
+						<br>
+						<p style="padding-left:20px;">و بأدق تفاصيلها، الأمر الذي وفر بيئة عمل تقنية وفنية احترافية عصرية متطورة تديرها نخبة من الخبرات والكفاءات المتمرسة في مجال الطباعة وخدماتها وحلولها أعمال ما قبل وما بعد الطباعة وأعمال وتقنيات التغليف والتجليد الفني وأعمال تصميم وابتكار الأعمال الإبداعية ، مع التركيز الكبير على الحرص بشكل دائم وبشكل دقيق ومدروس على اتباع معايير دقيقة في اختيار المواد الأولية والتجهيزات الداخلة في جميع العمليات الفنية والتقنية في أعمال الشركة على اختلاف، بحيث تكون صديقة للبيئة، وبأداء يرتقي إلى أعلى معايير الجودة المعمول فيها عالمياً في هذا المجال. بالشكل الذي يحافظ على مكانتها الريادية في السوق السعودية.</p>
+						<br>
+						<p style="padding-left:20px;">رؤيتنا نضع متطلبات العميل وحاجاته المتنامية٫ أولى اهتماماتنا. سعياً لبناء منظومة عمل متكاملة، ترتقي بقطاع الطباعة الاحترافية والتغليف لتواكب أحدث ما توصلت إليه تكنولوجيا الصناعة العالمية الاحترافية في هذا المجال.</p>
+						<br>
+						<p style="padding-left:20px;">رسالتنا أن نعتمد أعلى معايير الشفافية والإبداع في جميع أعمالنا. ونلتزم الاحترافية والأمانة والدقة في أدائنا عبر كافة وحدات العمل لدينا، في جميع الظروف والأوقات. أن نوفر حزمة من الخدمات المتطورة المتكاملة التي تدعم تنفيذ جميع أنماط المشاريع بكل يسر واحترافية وسلاسة.</p>
+						<br>
+						<p style="padding-left:20px;">قيمنا السرعة نلتزم إنجاز جميع أعمالنا بالسرعة المطلوبة بكفاءة واحترافية عالية، تلبي متطلبات واحتياجات جميع العملاء بمختلف شرائحهم في كافة الأوقات.</p>
+						<br>
+						<p style="padding-left:20px;">الدقة نعتمد أعلى معايير الدقة المتناهية في جميع مراحل أعمال ما قبل وما بعد الطباعة والتغليف الفني لكافة المشاريع مهما كان حجمها، بالشكل الذي يلبي كافة متطلبات واحتياجات العملاء.</p>
+						<br>
+						<p style="padding-left:20px;">الإبداع نفخر بأننا نقدم حلولاً إبداعية وتصاميم مبتكرة يديرها نخبة من المصممين الاحترافيين والمواهب المتميزة في هذا المجال.</p>
+						<br>
+						<p style="padding-left:20px;">روح المبادرة نعمل على ابتكار العديد من الحلول الإبداعية المبتكرة التي تضفي المزيد من التكامل في تلبية احتياجات عملاءنا بالشكل الذي يتناسب والأعمال المطلوبة بالتكاليف الأقل</p>
+						<br>
+						<p style="padding-left:20px;">الشغف نبتكر كافة الوسائل والحلول الكفيلة بتطبيق أعلى معايير الجودة التي تحقق رضا جميع العملاء وتتناسب مع سياسة عمل الشركة في تنفيذ الأعمال والمشاريع في كافة الأوقات.</p>
+					</div>
+				<div id="footer">
+					2015 &copy; جميع الحقوق محفوظة
+					<br>مطابع الوفاء نجران
+				</div>
+	</div>
+	<script src="/web/20161014152657js_/http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script>
+		$(document).ready(function(){
+	
+			$('ul.tabs li').click(function(){
+				var tab_id = $(this).attr('data-tab');
+
+				$('ul.tabs li').removeClass('current');
+				$('.tab-content').removeClass('current');
+
+				$(this).addClass('current');
+				$("#"+tab_id).addClass('current');
+			})
+
+		})
+	</script>
+</body>
+</HTML>
+
+
+
+
+
+<!--
+     FILE ARCHIVED ON 15:26:57 Oct 14, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:21:25 Feb 22, 2017.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
