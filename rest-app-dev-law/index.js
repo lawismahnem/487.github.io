b231a162fb0059a3c9d3ef779ec84dfbf6b383eb
lawismahnem <lordlawrencesalazar@gmail.com>
2024-04-02 04:51:07 +0800

    json server

---
+++ rest-app-dev-law/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+app.use(cors());
+
+const {readdirSync} = require("fs")
+
+readdirSync("./routes").map((file) => app.use("/",require("./routes/" + file)))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html")
+})
+
+require('dotenv').config();
+const port = process.env.PORT || 8000;
+
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
+})
